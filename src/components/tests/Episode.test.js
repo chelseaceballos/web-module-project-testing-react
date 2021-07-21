cf758f062677ca657fa67210f5b53840633c3ecc
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-07-21 12:43:55 -0700

    EpisodeTest Completed

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -4,38 +4,44 @@ import Episode from './../Episode';
 
 const testEpisode = {
     id:1,
-    name: "",
+    name: "test name",
     image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
     season: 1,
     number: 1,
-    summary: "",
+    summary: "test text1",
     runtime: 1
 }
 
-const testEpisodeWithoutImage = {
-    //Add in appropriate test data structure here.
-    // id:1,
-    // name: "",
-    // // image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
-    // season: 1,
-    // number: 1,
-    // summary: "",
-    // runtime: 1
+// Add in appropriate test data structure here.
+const testEpisodeWithoutImage = {    
+    id:2,
+    name: "test name",
+    image: null, 
+    season: 1,
+    number: 2,
+    summary: "test text2",
+    runtime: 45
 }
 
 test("renders without error", () => {
-//  render[<Episode />];
+ render(<Episode episode={testEpisode} />); 
 });
 
 test("renders the summary test passed as prop", ()=>{
-    // render[<Episode testProps={testEpisode.summary}/>];
-
+    render(<Episode episode={testEpisode} />);
+    let summary = screen.queryByText(/test text1/i)
+    expect(summary).toBeInTheDocument();
+    expect(summary).toBeTruthy();
+    expect(summary).not.toBeFalsy();
 });
 
 test("renders default image when image is not defined", ()=>{
-    // render[<Episode testProps={testEpisodeWithoutImage}/>];
-
-})
+    render (<Episode episode={testEpisodeWithoutImage}/>)
+    const image = screen.queryByAltText('./stranger_things.png');
+    expect(image).toBeInTheDocument();
+    expect(image).toBeTruthy();
+    expect(image).not.toBeFalsy();
+});
 
 //Tasks
 //1. Complete a test that shows the Episode component renders. Pass in the provided example episode data as a test prop.
